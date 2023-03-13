35d936206ff6161ee92244571e48c0f611db5cce
Finegan <126989239+fineg4n@users.noreply.github.com>
2023-03-12 22:33:57 -0300

    Create createLabs.py

---
+++ createLabs.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+
+import os
+
+
+path = '/home/' + os.getlogin() + '/labs'
+
+def welcome():
+    welc = 'Create Labs by F1neg4n'
+    os.system('clear')
+    print(welc + '\n' + '-' * len(welc) + '\n')
+    return
+
+def newLabName():
+    welcome()
+    try:
+        nameLab = input('[+] Enter the Lab name: ')
+        while(nameLab == ''):
+            nameLab = input('[+] Please, enter the Lab name: ')
+    except(KeyboardInterrupt):
+        os.system('clear')
+        welcome()
+        print('[-] Interrupted by user!')
+        exit()
+    return nameLab
+
+def createFolders():
+    nameLab = newLabName()
+    print('[+] Creating work environment "' + nameLab + '"...')
+    if(os.path.exists(path + '/' + nameLab + '/') == False):
+        os.system('mkdir ' + nameLab)
+        os.chdir(nameLab + '/')
+        os.system('mkdir 1.-Reserch 2.-Scripts 3.-Evidence')
+        print('[+] Done, Work environment succefully created!')
+    else:
+        print('[-] Sorry but "' + nameLab + '" already exists')
+        exit()
+    return
+
+def createEnvironment():
+    if(os.path.exists(path) == True):
+        os.chdir(path)
+        createFolders()
+    else:
+        os.system('mkdir ' + path)
+        os.chdir('labs/')
+        createFolders()
+    return
+
+if __name__ == '__main__':
+    createEnvironment()
